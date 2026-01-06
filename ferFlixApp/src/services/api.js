1f5eb33be3e68695820c63a066fad345dc861918
Marcos Lú <mlu.98@outlook.com>
2026-01-06 16:35:30 -0300

    Iniciando o consumo de api externa (Projetinho)

---
+++ ferFlixApp/src/services/api.js
@@ -0,0 +1,14 @@
+// base: https://sujeitoprogramador.com/
+
+// rota: r-api/?api=filmes
+
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://sujeitoprogramador.com/'
+})
+
+export default api
+
+
+
